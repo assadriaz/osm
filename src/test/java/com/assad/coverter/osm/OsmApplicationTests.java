a7649e2ce960ed067500cd19a5d2a709d8304731
Assad Riaz <assad.riaz@entur.org>
2024-12-11 14:14:26 +0100

    Intial commit

---
+++ src/test/java/com/assad/coverter/osm/OsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assad.coverter.osm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
